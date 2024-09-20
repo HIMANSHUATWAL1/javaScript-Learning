9ff4250147149aa7bb550af296c37479aa81e0dd
Himanshu Atwal <114054645+HIMANSHUATWAL1@users.noreply.github.com>
2024-09-20 23:41:27 +0530

    Update variable.js

--- variable.js
+++ variable.js
@@ -1,12 +1,13 @@
 const accountId = 144453
 let accountEmail="herdnj@gmail.com"
 var accountpassword="12345"
-accountCity="Jaipur"
-let accountState
+accountCity="Jaipur"  // we can declaire variable in this method but it is not a good approch to declaire the variable.
+let accountState ;   // undefined (not defined till now)
 /*
 prefer not to use var
 because of issue in block scope and functional scope
 */
+// {}---> scope 
 // accountId=2
 accountEmail="hc@gmail.com"
 accountCity="bengluru"
