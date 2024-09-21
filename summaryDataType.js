0c8c0c8c5bf49417b99d749c3c8e82336a3e614a
Himanshu Atwal <114054645+HIMANSHUATWAL1@users.noreply.github.com>
2024-09-22 00:22:06 +0530

    Update summaryDataType.js

--- summaryDataType.js
+++ summaryDataType.js
@@ -3,11 +3,11 @@
 Based on "how data resides on memory and how it is accessed" ,
 there are two type of datatype--->
 
-1.Primitive(call by value)
+1.Primitive(call by value) // changes on copy of these datatype.
 
 (String,Number,Boolean,null,BigInt,undefined,symbol )
 
-2.non-primitive(referenc-type)
+2.non-primitive(referenc-type)  // directly allocated the reference of these datatypt in memory 
 
 (Arrays,Objects,Functions)
 
@@ -31,7 +31,7 @@ console.log(id===anotherId) //false
 
 */
 
-// Array,Object,Function----->(return type of all --->object)
+// Array,Object,Function----->(return type of all --->object also known as function object)
 
 const heros=["saktiman","nagraj","vikral","krish"]
 let obj={
@@ -43,5 +43,18 @@ const myFunction=function(){
     console.log("hello world!")
 }
 
+/*
+Return type of all datatypes-->
+
+
+String-->string
+Boolean-->Boolean
+Number-->Number
+null-->Object
+undefined-->undefined
+Array-->object
+function-->function object
+Object-->object
+*/
 
 
