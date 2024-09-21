37d65c9ccffc22238e352e7ea7d20b31a58914c8
Himanshu Atwal <114054645+HIMANSHUATWAL1@users.noreply.github.com>
2024-09-21 23:56:13 +0530

    Update operations.js

--- operations.js
+++ operations.js
@@ -17,7 +17,7 @@ let negvalue=-value
 let str1="hello"
 let str2=" hem"
 let str3=str1+str2
-//console.log(str3)
+//console.log(str3)  // hello hem
 
 //console.log("1"+2)//12  if string first then all after treated as string
 //console.log(1+"2")//12
@@ -25,8 +25,8 @@ let str3=str1+str2
 //console.log(1+2+"2")//32 if first number all after treated as number
 
 
-//console.log(+true) // true+ not allowed
-//console.log(+"")
+//console.log(+true) // 1   // true+ not allowed
+//console.log(+"")   // 0
 
 
 /*
@@ -34,10 +34,16 @@ not a good choice to declaire variable because it harms readability of code
 let num1,num2,num3
 num1=num2=num3=2 + 2
 */
+let x=3;
+const y=x++;
+// so here  value of x=4 and value of y=3; 
 
+let x=3;
+const y=++x;
+// so here  value of x=4 and value of y=4; 
 
 let gamecounter=100
-//gamecounter++
-++gamecounter
+//gamecounter++    //101
+++gamecounter      //101
 
 console.log(gamecounter)
