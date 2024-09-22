c1f8dec4c73380ba479e4a1a0bcc0f5c77f36761
Himanshu Atwal <114054645+HIMANSHUATWAL1@users.noreply.github.com>
2024-09-23 01:06:53 +0530

    Update string.js

--- string.js
+++ string.js
@@ -3,11 +3,12 @@ const repoCount=50
 const gameName=new String("raja-fc-hey-hii")
 
 //console.log(name + repoCount + " value")
-
+// string Interpolation  (    ` ${} `--->placeholders  )
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
 
 //console.log(gameName.__proto__)--->{}
 
+//console.log(gameName[0])   // r
 //console.log(gameName.toUpperCase())
 //console.log(gameName.charAt(4))
 //console.log(gameName.indexOf('f'))
@@ -29,7 +30,7 @@ const url="https://himraj.com/raja%20moji"
 
 //console.log(url.replace('%20','-'))
 
-//console.log(url.includes('hey'))
+//console.log(url.includes('hey'))  //false
 
 console.log(gameName.split('-'))
 
